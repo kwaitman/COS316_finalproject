8eb6e089bbd95489ed7778bcf7ccb99338f8a101
Kayla Waitman <kw2689@princeton.edu>
2023-12-10 14:48:48 -0500

    working version using built in auth library

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, redirect, url_for, flash, request, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
+from models import db, User
+from config import Config
+from dotenv import load_dotenv
+
+load_dotenv()
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+app.app_context().push()
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful. Please login.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            flash('Login successful!', 'success')
+            # Implement session handling (e.g., Flask-Login) here
+            return redirect(url_for('loggedin'))
+        else:
+            flash('Login failed. Check your username and password.', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/loggedin')
+def loggedin():
+    return render_template('loggedin.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)  # Remove the user_id from the session
+    flash('Logout successful!', 'success')
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
