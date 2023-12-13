32e18431a5a2727f95384e50e88b1161e343ca89
Kayla Waitman <kw2689@princeton.edu>
2023-12-13 18:27:38 -0500

    added two new encryptions, but algorithms are not implemented yet

---
+++ caesar.py
@@ -0,0 +1,16 @@
+SHIFTVALUE=7
+alphabet=" abcdefghijklmnopqrstuvwxyz"
+# Generates an encrypted password given an inputted string
+def encrypt(password):
+    # do this algorithm
+    return encrypted
+
+def decrypt(encrypted):
+    # do this algorithm
+    return decrypted
+
+if __name__ == '__main__':
+    encrypted = encrypt("Hello World")
+    print(encrypted)
+    decrypted = decrypt(encrypted)
+    print(decrypted)
