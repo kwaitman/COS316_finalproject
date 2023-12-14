79bed70b6229ce50fd4c27b8d68688e2c9446d28
Kayla Waitman <kw2689@princeton.edu>
2023-12-14 15:00:26 -0500

    updated website to account for all the algorithms

--- models.py
+++ models.py
@@ -1,17 +1,26 @@
 from flask_sqlalchemy import SQLAlchemy
-from vigenere import encrypt, decrypt
+import vigenere
+import caesar
+import rc4
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_encrypted = db.Column(db.String(128), nullable=False)
+    password_v_encrypted = db.Column(db.String(128), nullable=False)
+    password_c_encrypted = db.Column(db.String(128), nullable=False)
+    password_r_encrypted = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
-        self.password_encrypted = encrypt(password)
+        self.password_v_encrypted = vigenere.encrypt(password)
+        self.password_c_encrypted = caesar.encrypt(password)
+        self.password_r_encrypted = rc4.encrypt(password)
+       
 
     def check_password(self, password):
-        if (decrypt(self.password_encrypted)) == password:
+        if ((vigenere.decrypt(self.password_v_encrypted)) == password) and \
+            ((caesar.decrypt(self.password_c_encrypted)) == password) and \
+            ((rc4.decrypt(self.password_r_encrypted)) == password):
             return True
         return False
